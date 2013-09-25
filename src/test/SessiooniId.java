5952efd32f9897fbeb14135f6018fe6562d20ff6
Kirss <qpenassi@hotmail.com>
2013-09-25 22:08:30 +0300

    Projekti teise osa esimene ülesanne

---
+++ src/test/SessiooniId.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//Esimene servlet n‰itab kasutaja sessiooni id-d ja sessiooni pandud atribuudi v‰‰rtust.
+//See vıimaldab ka sessiooni atribuuti seada. Seadmiseks on servleti parameeter nimega "param".
+//N‰ide: http://ci.itcollege.ee/part2example/HomePage
+
+public class SessiooniId extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello");
+		
+		String id = request.getRequestedSessionId();
+		response.getWriter().println("Your session id is  "+ id);
+		
+		Integer param = null;
+		request.getSession().setAttribute("param", param); 
+		
+		response.getWriter().println("Session attribute is " + param);
+	}
+
+	
+}
