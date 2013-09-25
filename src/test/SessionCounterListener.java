240529dc5560a0865cadd964147de6d87ba7cd82
Kirss <qpenassi@hotmail.com>
2013-09-25 22:32:56 +0300

    lisasin listeneri, et seda kasutada servleti SessioonideArv juures. Enne seda lõin SessioonideArv servleti ka.

---
+++ src/test/SessionCounterListener.java
@@ -0,0 +1,25 @@
+package test;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+
+public class SessionCounterListener implements HttpSessionListener {
+
+    private static int totalActiveSessions=0;
+   
+
+    public void sessionCreated(HttpSessionEvent event) {
+        totalActiveSessions++;
+    }
+
+	
+    public void sessionDestroyed(HttpSessionEvent event) {
+    	totalActiveSessions--;
+    }
+    
+    public static int getTotalActiveSessions() {
+    	return totalActiveSessions;
+    }
+	
+}
