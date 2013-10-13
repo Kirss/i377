3d1b772e4b8b6cdcde401e3f85d6ae965e9f739d
Kirss <qpenassi@hotmail.com>
2013-10-13 23:15:03 +0300

    see värk ei tööta veel

--- src/teineosa/servlets/Add.java
+++ src/teineosa/servlets/Add.java
@@ -1,37 +1,42 @@
 package teineosa.servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import teineosa.Dao;
 
 /**
  * Servlet implementation class Add
  */
 public class Add extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Add() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.getRequestDispatcher("WEB-INF/Add.jsp").forward(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String code = request.getParameter("code");
+
+		try {
+			new Dao().addItem(name, code);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		response.sendRedirect("Search");
 	}
 
 }
--- src/teineosa/servlets/ClearData.java
+++ src/teineosa/servlets/ClearData.java
@@ -1,37 +1,51 @@
 package teineosa.servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import teineosa.Dao;
 
 /**
  * Servlet implementation class Delete
  */
 public class ClearData extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ClearData() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		deleteItems(request);
+		response.sendRedirect("Search");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+
+	private void deleteItems(HttpServletRequest request) {
+		try {
+			if (request.getParameter("id").equalsIgnoreCase("All")) {
+				new Dao().deleteAll();
+			} else {
+				int id = Integer.parseInt(request.getParameter("id"));
+				new Dao().deleteItem(id);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
