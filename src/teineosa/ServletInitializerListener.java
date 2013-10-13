abcc0e8aa2332e5d258d70ffb6b894c50fa076ad
Kirss <qpenassi@hotmail.com>
2013-10-13 20:11:09 +0300

    andmebaasi failid ja vajaminevad servletid (pole valmis)

---
+++ src/teineosa/ServletInitializerListener.java
@@ -0,0 +1,29 @@
+package teineosa;
+
+import java.io.File;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import teineosa.SetupDao;
+
+public class ServletInitializerListener implements ServletContextListener{
+
+   @Override
+   public void contextInitialized(ServletContextEvent arg0) {
+      SetupDao setupDao = new SetupDao();
+
+      if(!databaseExists()) {
+         setupDao.createSchema();
+         setupDao.addTestData();
+      }
+   }
+
+   @Override
+   public void contextDestroyed(ServletContextEvent arg0) {      
+   }
+
+   private boolean databaseExists() {
+      return new File(System.getProperty("user.home") + "/data/Kirss/db.script").exists();
+   }
+}
